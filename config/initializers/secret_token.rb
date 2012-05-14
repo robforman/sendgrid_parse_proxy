9da917414b7825871b63f80c33327e5419710902
Rob Forman <rob.forman@hireiqinc.com>
2012-05-14 18:40:52 -0400

    initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SendgridParseProxy::Application.config.secret_token = '3f979db74fee1153a5dcfe9e1f06a287486c2a279142694fa274e8e04e7baa7864c04d3ca485a657346a0a6f4822e64f829f3c757e7f35360fa2b60e83961522'
